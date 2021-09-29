d2377cba2ffc1beca35aacaa4579e896742f7ee6
01081422peru <90700682+01081422peru@users.noreply.github.com>
2021-09-29 16:53:54 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+﻿// Lab_03_4.cpp
+// < Перун Уляна >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 16.
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	// розгалуження в повній формі
+	if (x >= 0 && y >= 0 && y <= -x * x + 2 && y <= x || x < 0 && y < 0 && y >= x && y <= -x * x + 2)
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+	return 0;
+}
